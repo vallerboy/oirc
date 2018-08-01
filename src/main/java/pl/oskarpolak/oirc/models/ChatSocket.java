a65ec2b324789efa41983c64e98dd0d96beb6a48
Oskix <oskar.polak@akademiakodu.pl>
2018-08-01 11:24:46 +0200

    test

--- src/main/java/pl/oskarpolak/oirc/models/ChatSocket.java
+++ src/main/java/pl/oskarpolak/oirc/models/ChatSocket.java
@@ -1,12 +1,16 @@
 package pl.oskarpolak.oirc.models;
 
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@EnableWebSocket
+@Component
 public class ChatSocket extends TextWebSocketHandler implements WebSocketConfigurer {
 
     @Override
